0997fa05ba2165fd4f308316ef2800d44197a7e1
manigedit <1manish22@gmail.com>
2019-12-22 09:57:56 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,111 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
+const mongoose = require('mongoose');
+
+const Movie = require('./models/movie');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use('/', router);
+app.use(express.static('frontend'))
+
+router.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/frontend/index.html'));
+  });
+
+app.use(
+  '/api',
+  graphqlHttp({
+    schema: buildSchema(`
+        type Movie{
+            _id: ID!
+            title: String!
+            locations: [String]!
+            production_company: String
+            director: String
+            actor1: String
+            actor2: String
+            actor3: String
+        }
+
+        input MovieInput {
+            title: String!
+            locations: [String]!
+            production_company: String
+            director: String
+            actor1: String
+            actor2: String
+            actor3: String
+        }
+
+        type RootQuery {
+            movies(searchby: String, like: String): [Movie]
+        }
+
+        type RootMutation {
+            createMovie(movieInput: MovieInput): Movie
+        }
+        
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+      movies: args => {
+        var queryParam = {};
+        var sub = new RegExp(args.like.toString(), 'i');
+        queryParam[args.searchby] = sub;
+        return Movie.find(queryParam).then( movies => {
+            return movies.map(movie =>{
+                return {...movie._doc};
+            });
+        })
+        .catch(err =>{
+            console.log(err);
+        })
+      }
+    ,
+    createMovie: args => {
+        const movie = new Movie({
+            title: args.movieInput.title,
+            locations: args.movieInput.locations,
+            production_company: args.movieInput.production_company,
+            director: args.movieInput.director,
+            actor1: args.movieInput.actor1,
+            actor2: args.movieInput.actor2,
+            actor3: args.movieInput.actor3
+        });
+
+        return movie
+            .save()
+            .then(result => {
+                console.log(result);
+                return {...result._doc};
+            })
+            .catch(err => {
+                console.log(err);
+                throw err;
+            });
+    }
+},
+
+    graphiql: true
+  })
+);
+
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USERID}:${process.env.MONGO_PASSWORD}@cluster0-wmcx9.mongodb.net/moviestab?retryWrites=true&w=majority`
+).then(()=>{
+
+    console.log('Connected');
+    app.listen(process.env.PORT || 4000);
+}).catch(err=>{
+    console.log(err);
+});
+
---
+++ frontend/js/main.js
@@ -0,0 +1,102 @@
+$(document).ready(() => {
+  $('#searchForm').on('submit', (e) => {
+    let searchText = $('#searchText').val();
+    getMovies(searchText);
+    e.preventDefault();
+  });
+});
+
+$('.carousel.carousel-multi-item.v-2 .carousel-item').each(function(){
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+
+  for (var i=0;i<4;i++) {
+    next=next.next();
+    if (!next.length) {
+      next=$(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+  }
+});
+
+function sleep(milliseconds) {
+  const date = Date.now();
+  let currentDate = null;
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < milliseconds);
+}
+
+var moviesWithLocation;
+function getMovies(searchText){
+
+const query = `
+    query getMovies($searchTxt: String!) {
+      movies(searchby: "title",like: $searchTxt) {
+        title
+        locations
+      }
+    }
+  `
+const variables = {
+  searchTxt: String(searchText)
+}
+
+
+
+fetch(`https://movie-mania-graphql.herokuapp.com/api`, {
+        method: 'POST',
+        json: true,
+        encoding: 'utf8',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({query, variables}),
+
+      })
+        .then(function(response){
+          return response.json();
+        })
+        .then( function(data){
+          console.log(data.data.movies);
+          moviesWithLocation = data.data.movies;
+          let output = '';
+          if (moviesWithLocation.length < 1){
+              output += `    <button type="button" class="btn btn-danger" href="#">OOPS No Results Found</button>    `;            
+          }
+          if (moviesWithLocation.length < 25){
+          $.each(moviesWithLocation, (index, movie) =>{
+            
+            output += `    <button id="'${index}'"" onclick="movieSelected(${index})" type="button" class="btn btn-warning" href="#">${movie.title}</button>    `;
+
+          });
+        }
+        if (moviesWithLocation.length > 24) {
+          output += `    <button type="button" class="btn btn-danger" href="#">Too many results Please try a detailed search</button>    `; 
+        }
+          
+
+
+          $('#moviebuttons').html(output);
+
+        });
+}
+
+function movieSelected(index){
+  let output = '';
+  var places = moviesWithLocation[index].locations;
+  $.each(places, (index, place) =>{
+    output += `<li class="list-group-item">${place}</li>`
+  });
+
+  $('#locationslist').html(output);
+
+
+
+  console.log("Clicked movies has");
+  console.log(moviesWithLocation[index].locations)
+}
+
